5f565a599f812ffd892ab6656ac184893cfa5140
jacques FRANC <jacquesn7@gmail.com>
2024-07-11 23:36:30 +0200

    some more cleaning

--- example/textmate/files/python_example.py
+++
@@ -1,9 +0,0 @@
-import foo
-
-
-async def bar():
-    f = await foo()
-    f_string = (
-        f"Hooray {f}! format strings are not supported in current Monarch grammar"
-    )
-    return f_string
